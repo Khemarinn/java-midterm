b4348781b51518af6cdf54850112dfe3f7d19070
Khemarinn <156316805+Khemarinn@users.noreply.github.com>
2024-01-11 18:57:36 +0700

    Add files via upload

---
+++ Pure Vistual Function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class area{
+    double dim1,dim2;
+public: void setarea(double a,double b)
+    {
+        dim1=d1;
+        dim2=d2;
+    }
+    void getdim(double &d1,double &d2)
+    {
+        dim1=d1;
+        dim2=d2;
+    }
+    virtual double
+};
